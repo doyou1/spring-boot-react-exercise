6a99a9b7166ec6ce866a960176246625e477bf72
doyou1 <56281493+doyou1@users.noreply.github.com>
2021-12-29 03:43:33 +0900

    demo initialize

---
+++ demo/src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller // (1)
+public class HomeController {
+    
+    @RequestMapping(value = "/")    // (2)
+    public String index() {
+        return "index"; // (3)
+    }
+}
