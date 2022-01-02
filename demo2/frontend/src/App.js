eb6b610703f89d6c6e5bb7b5e92adad54b3f618a
doyou1 <56281493+doyou1@users.noreply.github.com>
2022-01-02 21:38:47 +0900

    main page on 220102

--- demo2/frontend/src/App.js
+++ demo2/frontend/src/App.js
@@ -57,31 +57,100 @@ function TreeContainer() {
     <div>
       <div id="tree_container">
         <img id="tree_img" src={require("./img/christmas_tree_image.png")} />
-        <div class="tree_item_container">
-          <div class="tree_item_row1">
-            <div>abc</div>
-          </div>
-          <div class="tree_item_row2">
-            <div>ABC</div>
-            <div>DEF</div>
-            <div>DEF</div>
-          </div>
-          <div class="tree_item_row3">
-            <div>
-              123
-            </div>
-            <div>
-              456
-            </div>
-            <div>
-              789
-            </div>
-            <div>
-              789
-            </div>
-          </div>
-        </div>
+        <TreeItem />
+        <PageIndicator />
       </div>
+    </div>
+  );
+}
+
+function TreeItem() {
+  const treeColCounts = [1, 3, 4];
+  const itemSenders = [ "blank", "JH", "Chu", "IU", "Hyun"];
+
+  const rand_1_4 = () => {
+    return Math.floor(Math.random() * 4) + 1;
+  }
+
+  const makeTreeItem = () => {
+    const result = [];
+    for (let i = 0; i < treeColCounts.length; i++) {
+      const colCount = treeColCounts[i];
+      const cols = [];
+      for (let j = 0; j < colCount; j++) {
+        cols.push(
+          <div className="tree_item_col">
+            <div className="tree_item_sender">
+              {itemSenders[rand_1_4()]}
+            </div>
+            <div>
+              <img src={require(`./img/tree_item/item${rand_1_4()}.png`)} />
+            </div>
+          </div>
+        )
+      }
+      result.push(<div className={`tree_item_row${i+1}`}> {cols} </div>);
+    }
+
+    return result
+  }
+
+
+  return (
+    <div className="tree_item_container">
+      {makeTreeItem()}
+    </div>
+  );
+}
+
+function PageIndicator() {
+
+  const arrowNames = ["page_indicator_left", "page_indicator_right"]
+  
+  const makeLeftArrow = () => {
+    if(flag()) {
+      return (
+        <div className="page_indicator_item able">
+          <a href="#">&lt;&lt;</a>
+        </div>
+      );
+    } else {
+      return (
+        <div className="page_indicator_item disable">
+          <a href="#">&lt;&lt;</a>
+        </div>
+      );
+    }
+  }
+
+  const makeRightArrow = () => {
+    if(flag()) {
+      return (
+        <div className="page_indicator_item able">
+          <a href="#">&gt;&gt;</a>
+        </div>
+      );
+    } else {
+      return (
+        <div className="page_indicator_item disable">
+          <a href="#">&gt;&gt;</a>
+        </div>
+      );
+    }
+  }
+
+  const flag = () => {
+    return Math.random() < 0.5 ? true : false;
+  }
+
+
+  return (
+    <div id="page_indicator_container">
+      {makeLeftArrow()}
+      <div className="page_indicator_item page_indicator_text">
+        1/2
+      </div>
+      {makeRightArrow()}      
     </div>
   );
 }
